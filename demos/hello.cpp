1a4a1ee0207f4dae72617688a9bbb847bca9ed16
Fei Hao <mrfeihao@gmail.com>
2014-06-08 14:24:04 +1000

    renamed:    sdlpp/demo.cpp -> sdlpp/demos/hello.cpp

--- demos/hello.cpp
+++ demos/hello.cpp
@@ -1,4 +1,8 @@
 #include "sdlpp.hpp"
+#include "config.h"
+#include <string>
+#include <iostream>
+#define str(s) #s
 using namespace sdlpp;
 
 
@@ -6,13 +10,14 @@ int
 main
 (int argc, char *argv[])
 {
-    if (argc != 2) return 1;
     Handler sdl = Initializer().acquire();
     auto wm = WindowMode();
     auto window = sdl.createWindow("sdlpp_demo", Rectangular(800, 600),
             wm.opengl());
     auto dest = window.getSurface();
-    auto src = Surface::loadBMP(argv[1]);
+    auto path  = std::string(SDLPP_DEMO_DATA_DIR) + "hello.bmp";
+    std::cout << path << std::endl;
+    auto src = Surface::loadBMP(path);
     dest.blit(src);
     window.update();
     os::delay(20000);
